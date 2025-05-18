60f06729aa6382de7a56f865772c1bc4b1c8ce23
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-18 18:46:23 +0100

    Added ready and auto start features to lobby.

--- menus/lobby/lobby.gd
+++ menus/lobby/lobby.gd
@@ -1,17 +1,84 @@
 extends Control
 
+signal start_countdown()
+signal stop_countdown()
+
+var ready_players = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if multiplayer.is_server(): name = "server_host"
 	for player_id in MultiplayerManager.players.keys():
 		%PlayerList.add_player(player_id,MultiplayerManager.players[player_id])
-	MultiplayerManager.connect("player_connected",%PlayerList.add_player)
-	MultiplayerManager.connect("player_disconnected",%PlayerList.remove_player)
-	if not multiplayer.is_server():
-		%Start.queue_free()
+		ready_players[player_id] = false
+	MultiplayerManager.connect("player_connected",add_player)
+	MultiplayerManager.connect("player_disconnected",remove_player)
 
 func _on_disconnect_pressed():
 	MultiplayerManager.leave_server()
 
-func _on_start_pressed():
-	MultiplayerManager.load_scene.rpc("res://level/world.tscn")
+func _on_ready_pressed():
+	#Tells the host you are ready
+	player_ready.rpc_id(1,multiplayer.get_unique_id())
+
+func _on_unready_pressed():
+	player_unready.rpc_id(1,multiplayer.get_unique_id())
+
+func add_player(player_id,info):
+	ready_players[player_id] = false
+	%PlayerList.add_player(player_id,info)
+
+func remove_player(player_id):
+	ready_players.erase(player_id)
+	%PlayerList.remove_player(player_id)
+
+@rpc("any_peer","call_local","reliable")
+func player_ready(id):
+	#Only the server updates the player list
+	if not multiplayer.is_server(): return
+	
+	if not ready_players.has(id):
+		print("no id {val}".format({"val":id}))
+		print(ready_players)
+		return
+	
+	ready_players[id] = true
+	update_ready_list.rpc(ready_players)
+	check_ready.rpc()
+
+@rpc("any_peer","call_local","reliable")
+func player_unready(id):
+	#Only the server updates the player list
+	if not multiplayer.is_server(): return
+	
+	print("test {id} self {self}".format({"id":id,"self":multiplayer.get_unique_id()}))
+	if not ready_players.has(id):
+		print("no id {val}".format({"val":id}))
+		print(ready_players)
+		return
+	
+	ready_players[id] = false
+	update_ready_list.rpc(ready_players)
+	check_ready.rpc()
+
+@rpc("any_peer","call_local","reliable")
+func check_ready():
+	if ready_players.values().all(func(val): return val):
+		start_countdown.emit()
+	else:
+		stop_countdown.emit()
+
+#Updates others with the hosts list 
+@rpc("authority","call_local","reliable")
+func update_ready_list(list):
+	ready_players = list
+	for id in ready_players:
+		if ready_players[id]:
+			%PlayerList.get_displayed_label(id).modulate = Color.WEB_GREEN
+		else:
+			%PlayerList.get_displayed_label(id).modulate = Color.WHITE
+
+func start():
+	MultiplayerManager.load_scene.rpc("res://levels/test_world.tscn")
+
+func _on_start_delay_timeout():
+	start()
