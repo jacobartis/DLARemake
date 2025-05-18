3cd043e4ff20c27efc7daa89f69bb3e657b10fe0
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-18 19:35:33 +0100

    Added settings preload with mouse sence.

---
+++ preloads/settings.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var mouse_sense: float = 0.01
+
+
+func clamp_mouse():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func free_mouse():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
