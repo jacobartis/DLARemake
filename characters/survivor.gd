949434850475574b9b14d2743964432824cf073f
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-18 19:35:18 +0100

    Created a simple multiplayer player controller.

---
+++ characters/survivor.gd
@@ -0,0 +1,50 @@
+extends CharacterBody3D
+
+@onready var cam = $Camera3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@rpc("any_peer","call_local")
+func update_owner(id):
+	set_multiplayer_authority(id)
+	cam.current = is_authority()
+
+func is_authority():
+	return get_multiplayer_authority()==multiplayer.get_unique_id()
+
+func _input(event):
+	if not is_authority(): return
+	look(event as InputEventMouseMotion)
+
+func look(motion:InputEventMouseMotion):
+	if not motion or Input.mouse_mode!=Input.MOUSE_MODE_CAPTURED: return
+	cam.rotation.x -= motion.relative.y*Settings.mouse_sense
+	cam.rotation_degrees.x = clamp(cam.rotation_degrees.x,-70,80)
+	rotation.y -= motion.relative.x*Settings.mouse_sense
+
+func _process(delta):
+	if not is_authority(): return
+
+func _physics_process(delta):
+	if not is_authority(): return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
