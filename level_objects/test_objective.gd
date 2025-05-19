0c284828360be8f98edefbcae1a36a4aa7840336
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-19 18:13:25 +0100

    Fixed fill. Added buffer for press and delay before level dropping.

--- level_objects/test_objective.gd
+++ level_objects/test_objective.gd
@@ -4,22 +4,35 @@ extends Node3D
 @onready var fill:MeshInstance3D = $Fill
 
 @export var duration: float = 5
+@export var empty_rate: float = 2
 @export var obj_node: ObjectiveNode
 
-var last_press: float = -1
+var hold_time: float = -1
+var press_buffer: float = 0
+var hold_buffer: float = 0
 
 func _on_object_interact_is_held():
-	last_press = Time.get_ticks_msec()
+	press_buffer = 0.2
 
-func hold_time():
-	if last_press == -1: return 0
-	return Time.get_ticks_msec() - last_press
 
 func _process(delta):
 	if obj_node.complete: return
+	if press_buffer:
+		press_buffer = max(press_buffer-delta,0)
+		hold_time = max(hold_time+delta,0)
+		if press_buffer == 0:
+			hold_buffer = .5
+	elif hold_buffer > 0:
+		hold_buffer = max(hold_buffer-delta,0)
+	elif hold_buffer == 0:
+		hold_time = max(hold_time-delta*empty_rate,0)
 	var mesh = $Fill.mesh
-	var perc = clamp(hold_time()/duration,0,1) #% of duration held
+	var perc = clamp(hold_time/duration,0,1) #% of duration held
 	mesh.height = (perc*2.0)
 	$Fill.position.y = -1*(1-perc)
 	if perc == 1:
 		obj_node.finish()
+
+
+func _on_object_interact_player_entered(player):
+	print(player)
