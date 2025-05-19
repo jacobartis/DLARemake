17724f43bd8ecd560e1c14f4b0c5ff6303c9a51e
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-19 12:42:33 +0100

    Test objective, player hold interacts and the mesh bar grows.

---
+++ level_objects/test_objective.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@onready var fill:MeshInstance3D = $Fill
+
+@export var duration: float = 5
+
+var last_press: float = -1
+
+func _on_object_interact_is_pressed():
+	last_press = Time.get_ticks_msec()
+
+func hold_time():
+	if last_press == -1: return 0
+	return Time.get_ticks_msec() - last_press
+
+func _process(delta):
+	var mesh = $Fill.mesh
+	var perc = clamp(hold_time()/duration,0,1) #% of duration held
+	mesh.height = (perc*2.0)
+	mesh.position.y = -1*(1-perc)
