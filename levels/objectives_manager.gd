7fa830f760492c5c29030f016a52d94a6071f3fc
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-20 23:03:31 +0100

    Emits signal when any objective is completed.

--- levels/objectives_manager.gd
+++ levels/objectives_manager.gd
@@ -1,11 +1,13 @@
 extends Node
 
 signal survivors_win()
+signal objective_complete()
 
 func connect_obj(obj):
 	obj.completed.connect(on_objective_complete)
 
 
 func on_objective_complete():
+	objective_complete.emit()
 	if not get_tree().get_nodes_in_group("objective").all(func (o): return o.complete): return
 	survivors_win.emit()
