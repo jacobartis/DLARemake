b6467dfe69e8b5a5c92713235bc9239dd01cdc87
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-21 19:10:36 +0100

    Added spawn level trenchbroom object.

---
+++ level_objects/spawns/player_spawn.gd
@@ -0,0 +1,16 @@
+@tool
+extends Node3D
+class_name PlayerSpawn
+
+@export var type: String = "survivor"
+
+func _func_godot_apply_properties(entity_properties: Dictionary):
+	print(entity_properties)
+	if entity_properties.has("type"):
+		type = entity_properties["type"]
+
+func _func_godot_build_complete():
+	print("Build complete")
+
+func _ready():
+	add_to_group("spawn")
