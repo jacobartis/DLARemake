7386aad67506dd889a102cf7d667d62dabaf935e
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-21 19:09:25 +0100

    Moved exit level object.

--- level_objects/exits/exit.gd
+++ level_objects/exits/exit.gd
@@ -3,10 +3,12 @@ extends Node3D
 @onready var objective_node = $ObjectiveNode
 
 var survs_in = []
+var enabled: bool = false
 
 func _ready():
 	var manager = get_tree().get_first_node_in_group("objective_manager")
 	manager.objective_complete.connect(check_remaining)
+	$Highlight.hide()
 
 func check_remaining():
 	if get_tree().get_nodes_in_group("objective").filter(func (o): return o!=objective_node).all(func(o):return o.completed):
@@ -15,18 +17,21 @@ func check_remaining():
 func enable():
 	if GameInfo.role(multiplayer.get_unique_id())=="Survivor":
 		$Highlight.show()
+		enabled = true
 
 func check_all():
+	if not enabled: return
 	var surv = get_tree().get_nodes_in_group("survivor").filter(func (s): return not s.dead)
-	if surv.size() == survs_in:
+	if surv.size() == survs_in.size():
 		objective_node.finish.rpc()
 
+#TODO
+#Bug where players arent correctly removed on exit.
 func _on_player_detector_player_entered(player):
 	if survs_in.has(player): return
-	survs_in.append(survs_in)
+	survs_in.append(player)
 	check_all()
-
 
 func _on_player_detector_player_exited(player):
 	if not survs_in.has(player): return
-	survs_in.erase(survs_in)
+	survs_in.erase(player)
