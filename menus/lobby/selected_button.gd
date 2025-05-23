e181352af5765fd79cc86255d9fc4c0186298ac3
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-23 15:50:09 +0100

    Loading as client sends request to server for map id.

--- menus/lobby/selected_button.gd
+++ menus/lobby/selected_button.gd
@@ -1,14 +1,25 @@
 extends Button
 
+var level_id:String = "NOT YET SELECTED"
+
 func _ready():
 	if multiplayer.is_server(): return
 	disabled = true
-
-func _on_level_select_level_selected(level_id):
-	update_display.rpc(level_id)
+	update_clients.rpc_id(1)
 
 @rpc("any_peer","call_local","reliable")
-func update_display(level_id):
+func update_label(id):
+	level_id = id
+	update_display()
+
+@rpc("any_peer","call_local","reliable")
+func update_clients():
+	update_label.rpc(level_id)
+
+func _on_level_select_level_selected(id):
+	update_label.rpc(id)
+
+func update_display():
 	var info = GameInfo.level_infos[level_id]
 	%MapIcon.texture = info.level_background
 	%LevelName.text = info.level_name
