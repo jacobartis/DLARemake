4424397ecf1dd480baf82c3d73df53d869f2ba2f
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-23 15:49:25 +0100

    Sets environment depending on if killer of survivor.

---
+++ levels/environment_setter.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@export var killer: Environment
+@export var survivor: Environment
+
+var world_environment:WorldEnvironment
+
+func _ready():
+	world_environment = WorldEnvironment.new()
+	add_child(world_environment)
+
+@rpc("any_peer","call_local","reliable")
+func set_filter():
+	if GameInfo.role(multiplayer.get_unique_id()) == "Killer":
+		world_environment.environment = killer
+	else:
+		world_environment.environment = survivor
+
+func _on_level_template_game_start():
+	set_filter.rpc()
