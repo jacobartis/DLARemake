ffcf3b14a7fba7ecfa29de59d7f93f5396bc9372
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-25 11:11:26 +0100

    Added audio input for getting mic input.

---
+++ features/audio_input.gd
@@ -0,0 +1,35 @@
+extends Node
+
+@onready var input: AudioStreamPlayer = $Input
+@export var output :AudioStreamPlayer3D
+
+var idx : int
+var effect : AudioEffectCapture
+var playback : AudioStreamGeneratorPlayback
+
+func _ready():
+	playback = output.get_stream_playback()
+	#Tool only
+
+func _on_owner_updated():
+	# we only want to initalize the mic for the peer using it
+	if (is_multiplayer_authority()):
+		input.stream = AudioStreamMicrophone.new()
+		input.play()
+		idx = AudioServer.get_bus_index("VoiceIn")
+		effect = AudioServer.get_bus_effect(idx, 0)
+		# replace 0 with whatever index the capture effect is
+
+func _process(delta: float) -> void:
+	#if (not is_multiplayer_authority()): return
+	if (effect.can_get_buffer(512) && playback.can_push_buffer(512)):
+		send_data.rpc(effect.get_buffer(512))
+		#send_data(effect.get_buffer(512))
+	effect.clear_buffer()
+
+# if not "call_remote," then the player will hear their own voice
+# also don't try and do "unreliable_ordered." didn't work from my experience
+@rpc("any_peer", "call_remote", "reliable")
+func send_data(data : PackedVector2Array):
+	for i in range(0,512):
+		playback.push_frame(data[i])
