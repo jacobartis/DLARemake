e4762822a40c651a922f7bab3d2c622ec0ee8a22
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-25 20:32:58 +0100

    Added update pref funcs.

--- menus/lobby/player_list.gd
+++ menus/lobby/player_list.gd
@@ -8,11 +8,12 @@ var displayed_players = {}
 
 func add_player(id,info):
 	var disp = player_packed.instantiate()
-	add_child(disp)
+	add_child(disp,true)
 	disp.set_player(id,info)
 	if id != 1:
 		disp.set_kickable(multiplayer.get_unique_id()==1)
 	disp.get_kick_button().pressed.connect(kick_pressed.bind(id))
+	disp.get_pref_button().item_selected.connect(update_pref.bind(id))
 	displayed_players[id] = disp
 
 func kick_pressed(id):
@@ -26,3 +27,11 @@ func remove_player(id):
 func get_displayed_label(id):
 	if not displayed_players.has(id): return
 	return displayed_players[id]
+
+func update_pref(val,id):
+	remote_update.rpc(val,id)
+	displayed_players[id].set_selected_pref(val)
+
+@rpc("any_peer","call_remote","reliable")
+func remote_update(val,id):
+	displayed_players[id].set_selected_pref(val)
