74b87e717a03f09dc70828ef2844d2309d6c94ad
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-27 08:22:41 +0100

    Emits name of the action when selected. Swapping now must be done in mode.

--- characters/killer/actions/killer_action_handler.gd
+++ characters/killer/actions/killer_action_handler.gd
@@ -1,7 +1,12 @@
 extends Node3D
 class_name KillerActionHandler
 
+signal action_selected(action)
+signal entering_mode()
+signal exiting_mode()
+
 @export var actions:Array[KillerAction]
+@export var killer_picker:KillerAction
 @export var killer:CharacterBody3D
 
 var current_action:KillerAction
@@ -12,11 +17,18 @@ var mode: bool
 func is_current(val):
 	return current_action == val
 
+func _ready():
+	start_action(actions[selected_pos])
+
 func start_action(val):
 	if current_action: return
 	current_action = val
+	action_selected.emit(current_action.name)
+	current_action.enter_action()
 
 func end_action():
+	if current_action:
+		current_action.exit_action()
 	current_action = null
 
 func enter_mode():
@@ -25,6 +37,7 @@ func enter_mode():
 	mode = true
 	tween = get_tree().create_tween()
 	tween.tween_property(%CamArm,"spring_length",0,.2)
+	entering_mode.emit()
 
 func exit_mode():
 	if tween:
@@ -34,21 +47,23 @@ func exit_mode():
 	tween.tween_property(%CamArm,"spring_length",5,.2)
 	if current_action:
 		end_action()
+	exiting_mode.emit()
 
 func _process(delta):
 	if not killer.controlled or not killer.is_authority(): return
 	if Input.is_action_just_pressed("ActionMode"):
 		enter_mode()
+		start_action(actions[selected_pos])
 	elif Input.is_action_just_released("ActionMode"):
 		exit_mode()
-	
+	if not mode: return
 	#Swap between actions when not in mode
-	if not mode:
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_WHEEL_UP):
+	if not is_current(killer_picker):
+		if Input.is_action_just_released("NextAction"):
 			selected_pos = wrap(selected_pos+1,0,actions.size())
 			end_action()
 			start_action(actions[selected_pos])
-		else:
+		elif Input.is_action_just_released("PrevAction"):
 			selected_pos = wrap(selected_pos-1,0,actions.size())
 			end_action()
 			start_action(actions[selected_pos])
