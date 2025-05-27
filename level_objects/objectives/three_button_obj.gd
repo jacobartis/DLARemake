1d4a602b6611a13c96139444884bd67846e7c66d
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-27 08:24:09 +0100

    Made class, restricted action inputs to server.

--- level_objects/objectives/three_button_obj.gd
+++ level_objects/objectives/three_button_obj.gd
@@ -1,10 +1,12 @@
 @tool
 extends Node3D
+class_name ThreeButtonObj
 
 @export var s_man:Node3D
 @export var disable_timer:Timer
 @export var minigame:Minigame
 @export var objective_node:ObjectiveNode
+@export var highlight:HighlightManager
 
 @export var punish_dur:float
 
@@ -16,6 +18,7 @@ func _func_godot_apply_properties(entity_properties: Dictionary):
 
 @rpc("any_peer","call_local","reliable")
 func disable(durr):
+	if objective_node.complete: return
 	disable_timer.start(durr)
 	s_man.show_screen(1)
 
@@ -23,21 +26,24 @@ func is_disabled():
 	return not disable_timer.is_stopped()
 
 func _on_left_is_pressed():
+	if not multiplayer.is_server(): return
 	if is_disabled() or objective_node.complete: return
+	print(multiplayer.get_unique_id()," Left")
 	minigame.left()
 
 func _on_right_is_pressed():
+	if not multiplayer.is_server(): return
 	if is_disabled() or objective_node.complete: return
 	minigame.right()
 
 
 func _on_middle_is_pressed():
+	if not multiplayer.is_server(): return
 	if is_disabled() or objective_node.complete: return
-	minigame.middle.rpc()
+	minigame.middle()
 
 func _on_disable_timer_timeout():
 	s_man.show_screen.rpc(0)
-
 
 func _on_mini_game_fail():
 	fail_condition.rpc()
