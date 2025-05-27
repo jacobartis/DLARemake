a292b10333b5a5c91d1c73cf9f2c418a6e460e4c
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-05-27 08:26:00 +0100

    Added tween for rot.

--- level_objects/objectives/captcha/captcha_mini_game.gd
+++ level_objects/objectives/captcha/captcha_mini_game.gd
@@ -1,34 +1,43 @@
 extends Minigame
 
+@export var connected_obj: ThreeButtonObj
 @onready var obj = $Obj
-@onready var show = $Show
+@onready var example = $Show
 @export var turns = 4
 @onready var interval = 360/turns
 
+var tween: Tween
 var options = ["😀", "😂", "😎", "🥳", "🤖", "🧠", "🚀", "🌍", "🎯", "📚", "💡", "🔥", "🧪", "🎉", "🛠️", "🖼️", "🎮", "📈", "🕵️‍♂️", "💻"]
+var current_rot: Vector3 = Vector3(0,0,0)
 
 func _ready():
 	super()
+	if connected_obj.get("func_godot_properties"):
+		var obj_prop = connected_obj.get("func_godot_properties")
+		if obj_prop.has("turns"):
+			turns = obj_prop["turns"]
 	var choice = options.pick_random()
 	obj.text = choice
-	show.text = choice
-	obj.rotation_degrees.z = randi_range(1,turns-1)*interval
+	example.text = choice
+	current_rot.z = randi_range(1,turns-1)*interval
+	obj.rotation_degrees.z = current_rot.z
 
-@rpc("any_peer","call_local","reliable")
+func rot_tween(rot):
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_property(obj,"rotation_degrees",rot,.2)
+	print(tween)
+
 func left():
-	if not multiplayer.is_server(): return
-	obj.rotation_degrees.z += interval
-	print(obj.rotation_degrees.z)
+	current_rot.z += interval
+	rot_tween(current_rot)
 
-@rpc("any_peer","call_local","reliable")
 func right():
-	if not multiplayer.is_server(): return
-	obj.rotation_degrees.z -= interval
-	print(obj.rotation_degrees.z)
+	current_rot.z -= interval
+	rot_tween(current_rot)
 
-@rpc("any_peer","call_local","reliable")
 func middle():
-	if not multiplayer.is_server(): return
 	if int(round(obj.rotation_degrees.z))%360==0:
 		success.emit()
 	else:
